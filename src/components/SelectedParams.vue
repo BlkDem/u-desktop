fa84c5afb1405bb15cb27bad3e474071c4fde979
BlkDem <m_sp@mail.ru>
2023-05-12 15:20:07 +0300

    param form

--- src/components/SelectedParams.vue
+++ src/components/SelectedParams.vue
@@ -1,17 +1,21 @@
 <template>
     <div class="">
   <!-- <div class="p-2 border border-1 rounded-4"> -->
-  <div class="flex-left m-1 p-1 border border-1 rounded-2 text-white" v-for="(item, key) in items" :key="key">
+  <div class="flex-space m-1 py-1 px-2 border border-1 rounded-2 text-white" v-for="(item, key) in items" :key="key">
     <i class="fa-solid fa-microchip fa-2x text-info"></i>
-    <div class="p-2">{{ item.id }}</div>
-    <div>
+    <div class="p-2">{{ item.id }}
 
-      <a href="#" @click="removeItem(key)"
-
-      >
+      <a href="#" @click="setItemForm(key)">
         {{ item.param_name }}
       </a>
+
     <!-- </div> -->
+    </div>
+    <div>
+      <a href="#" @click="removeItem(key)">
+        <!-- <i class="fa-regular fa-trash-can fa-2x text-danger"></i> -->
+        <i class="fa-solid fa-xmark fa-2x text-danger"></i>
+      </a>
     </div>
   </div>
 </div>
@@ -22,7 +26,7 @@
 
 export default {
 
-  emits: ['removeItem'],
+  emits: ['removeItem', 'setItemForm'],
 
   props: {
     items: {
@@ -33,6 +37,10 @@ export default {
   methods: {
     removeItem(key) {
       this.$emit('removeItem', key)
+    },
+
+    setItemForm(key) {
+      this.$emit('setItemForm', this.items[key]);
     }
   }
 }
